da22461b5ba4c40565f46e2cec2caa0a0aad06f8
Sumaiya Khan <sumaiya.khan32665@gmail.com>
2023-08-08 23:10:13 +0600

    aaded 8

--- conecpt.js
+++ conecpt.js
@@ -153,9 +153,9 @@ function gemsToDiamond(x,y,z){
 }
 console.log(gemsToDiamond(100, 5, 1));
 
-// function name printDetails(). you will be provided an object. you have to use the properties of the object and make a meaning full sentence. if there are any property missing you have to print there (nai);
+// 7function name printDetails(). you will be provided an object. you have to use the properties of the object and make a meaning full sentence. if there are any property missing you have to print there (nai);
 
-/*7
+/*
 const obj = {
     name: "mehedy",
     age:26,
@@ -184,3 +184,31 @@ const obj = {
 }
 
 console.log(printDetails(obj));
+
+
+/*
+8:
+আপনার কাছে একটি জাভাস্ক্রিপ্ট ফাংশন রয়েছে যার নাম হলো isValidPhotoName(photoName, imageExtensions)। এই ফাংশনটি দুটি প্যারামিটার গ্রহণ করে: photoName, যা একটি ছবির নাম প্রদর্শন করে, এবং imageExtensions Array, যা .jpg, .png, ইত্যাদি ছবির এক্সটেনশন গুলি ধারণ করে।
+আপনার কাজ হলো isValidPhotoName ফাংশনটি সম্পূর্ণ করা যাতে এই ফাংশনের মাধ্যমে পরীক্ষা করা যায় আপনি দেওয়া প্যারামিটার photoName সঠিক ছবির নাম কিনা তা। এই ফাংশনের কাজ হলো true রিটার্ন করা যদি photoName একটি বৈধ ছবির নামের শেষে বিদ্যমান ছবির এক্সটেনশন গুলির মধ্যে যেকোনটি শেষ হয়; অন্যথায়, ফাংশনটি false রিটার্ন করবে।
+
+*/
+
+// .jpg .jpeg .png .gif .ico .JPEG
+
+function isValidPhotoName(photoName, imageExtensions){
+    if(typeof photoName !== "string" || !Array.isArray(imageExtensions)){
+        return "please provide valid inputs"
+    }else{
+        for(let item of imageExtensions){
+           if(photoName.toLowerCase().endsWith(item.toLowerCase())){
+                return true;
+           }
+        }
+        return false;
+    }
+
+}
+
+
+const photo = "image.svg";
+const extensions = [".jpg",".JPEG",".png",".gif",".ico"];
