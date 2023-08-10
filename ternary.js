4ee8b1a20e1c66f69a3ed4acfc5d6d0cb68543a9
Sumaiya Khan <sumaiya.khan32665@gmail.com>
2023-08-10 21:40:44 +0600

    tenary

--- ternary.js
+++ ternary.js
@@ -34,17 +34,75 @@ function loop() {
 
 console.log(loop());
 
-//3--------nested if and else if
+//-------nested if and else if
+
+///3-------find if string2 includes in string1
 
 function matchFinder(string1, string2) {
-return  typeof string1 !== "string" || typeof string2 !== "string"
+  return typeof string1 !== "string" || typeof string2 !== "string"
     ? "Please enter two valid strings."
     : string1.includes(string2)
     ? true
     : false;
 }
-console.log(matchFinder("sumaiya","iya"));
+
+//4--------print a or b
+console.log(matchFinder("sumaiya", "iya"));
 function hello(a, b) {
   return a > b ? a : b;
 }
 console.log(hello(4, 6));
+
+//5---------if [3,4] =[4,3] ,[-9,9]=invalid ,[1,1]=equal
+// function sortMaker(arr) {
+//     if(!Array.isArray(arr)){
+//         return "Please Enter Required Array.";
+//     }
+//     else if(arr.length!==2){
+//         return "Please Enter An Array of Index 2.";
+//     }
+//     else{
+//         let a=arr[0];
+//         let b=arr[1];
+//         if(typeof a!=='number' || typeof b!=='number'){
+//             return "Please enter two numbers in the array.";
+//         }
+//         else if(a<0 || b<0){
+//             return "Invalid Input";
+//         }
+//         else if(a===b){
+//             return "equal";
+//         }
+//         else{
+//             if(a<b){
+//                const temp=a;
+//                a=b;
+//                b=temp;
+//                arr=[a,b];
+//                return arr;
+//             }
+//         else{
+//             return arr;
+//             }
+//         }
+//     }
+// }
+
+function sortMaker(arr) {
+    let temp=arr[0];
+    arr[0]=arr[1];
+    arr[1]=temp;
+   let arr2=[arr[0],arr[1]];
+  return (!Array.isArray(arr))
+    ? "Please Enter Required Array."
+    : arr.length !== 2
+    ? "Please enter array of index 2"
+    : typeof arr[0] !== "number" || typeof arr[1] !== "number"
+    ? "Enter two numbers"
+    : arr[0] < 0 || arr[1] < 0
+    ? "Invalid"
+    : arr[0] === arr[1]
+    ? "Equal"
+    : arr[0]<arr[1]? arr2:arr;
+}
+console.log(sortMaker([4,5]));
