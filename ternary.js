4f6631a59706a9ff148d70b90d2c5faf5b9898c2
Sumaiya Khan <sumaiya.khan32665@gmail.com>
2023-08-10 21:24:39 +0600

    addeec ternary

--- ternary.js
+++ ternary.js
@@ -17,7 +17,7 @@ for (let i = 1; i <= 50; i++) {
 }
 //with a function
 function loop() {
-    let array=[]
+  let array = [];
   for (let i = 1; i <= 50; i++) {
     let p =
       i % 3 == 0 && i % 5 === 0
@@ -27,11 +27,24 @@ function loop() {
         : i % 3 == 0
         ? "Bar"
         : i;
-        array.push(p);
-        
+    array.push(p);
   }
-  return array;}
-
-
+  return array;
+}
 
 console.log(loop());
+
+//3--------nested if and else if
+
+function matchFinder(string1, string2) {
+return  typeof string1 !== "string" || typeof string2 !== "string"
+    ? "Please enter two valid strings."
+    : string1.includes(string2)
+    ? true
+    : false;
+}
+console.log(matchFinder("sumaiya","iya"));
+function hello(a, b) {
+  return a > b ? a : b;
+}
+console.log(hello(4, 6));
